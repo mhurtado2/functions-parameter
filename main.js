a0a1fab5bebac36cf39fd14a6dbe209815f286e9
Shane <mhurtado1@berklee.edu>
2022-12-12 09:40:50 -0600

    func paramater

---
+++ main.js
@@ -0,0 +1,16 @@
+const quarterValue = (value) => {
+    // make a variable that is the value quarted
+    const quarter = value / 4
+
+
+    // Invoke the function and store the return value
+    //return the value of quarter
+    return quarter
+}
+
+// make a variable that calls the function with an argument
+
+const returnedNumber = quarterValue(100)
+
+// Log the return
+console.log(returnedNumber)
